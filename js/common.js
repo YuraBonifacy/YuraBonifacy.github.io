e95fb86f0d6e7ecb3222c5c0efb8fcd0bd73dcc7
Yura <43784885+YuraBonifacy@users.noreply.github.com>
2019-05-31 14:00:32 +0300

    Update all files

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function ($) {
 	$('#menu').on('click', function () {
+		$('html').css('overflow', 'hidden');
 		$('.mobile-menu').toggle();
+	});
+
+	$('.mobile-menu').on('click', function(){
+		$(this).toggle();
 	});
 });
